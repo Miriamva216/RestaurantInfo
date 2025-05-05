28671ae61d4e56fa7cbeda0886c94871dbf43145
Miriamva216 <miriam.armendariz@csuglobal.edu>
2025-05-04 19:15:54 -0600

    Add files via upload

---
+++ RestaurantInfo.java
@@ -0,0 +1,13 @@
+public class RestaurantInfo {
+    public static void main(String[] args) {
+        String restaurantName = "The Rabbit Hole";
+        String address = "101 N Tejon St";
+        String city = "Colorado Springs";
+        String state = "CO";
+        String zip = "80903";
+
+        System.out.println(restaurantName);
+        System.out.println(address);
+        System.out.println(city + ", " + state + " " + zip);
+    }
+}
